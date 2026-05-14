5414ef33a0ed7447bc5905e2b6f56d074bf757e3
GustavoMF-create <gustavomullerferreira5@gmail.com>
2026-05-13 21:14:56 -0300

    Initial Commit

---
+++ teste_python.py
@@ -0,0 +1,49 @@
+#========================
+# CALCULADORA EM PYTHON
+#========================
+
+def soma(a,b):
+    return a + b
+
+def multiplicacao(a,b):
+    return a * b
+
+def divisao(a,b):
+    if b == 0:
+        return "Div0"
+    else:
+        return a / b
+
+def subtracao(a,b):
+    return a - b
+
+
+while True:
+    print("\n======== Calculadora =========")   
+    print("1-Soma")
+    print("2-Multiplicação")
+    print("3-Divisão")
+    print("4-Subtração")
+    print("0-Sair")
+    
+    opcao = int(input("Escolha o que deseja fazer: "))
+    
+    if opcao == 0:
+        break
+    
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    
+    if opcao == 1:
+        resultado = soma(num1,num2)
+        print(f"Resultado = {resultado}")
+    elif opcao == 2:
+        resultado = multiplicacao(num1,num2)
+        print(f"Resultado = {resultado}")
+    elif opcao == 3:
+        resultado = divisao(num1,num2)
+        print(f"Resultado = {resultado}")
+    elif opcao == 4:
+        resultado = subtracao(num1,num2)
+        print(f"Resultado = {resultado}")
+    
